2e31be6055e955f4b82daac8c86e269218363be5
mario <mariovanbuynderen@gmail.com>
2023-06-12 10:52:12 +0200

    test 1er push

---
+++ connexion.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Veuillez vous identifier
+Entrez votre identifiant :"
+#id = 
+#fonction verifId
+#if []
+
+echo "Entrez votre mot de passe :"
+#fonction verifMDP
+#password =
+
+#if []
+
+
+echo "Connexion autorisée
+Bienvenue sur votre bureau de vote en ligne.
+Menu :
+1 : Voter en votre nom 
+2 : Voter pour quelqu'un d'autre (procuration)
+3 : Observer l'urne
+4 : Voir la liste électorale de votre ville
+5 : Voir le compteur de votes
+6 : Créer une procuration
+7 : Déclarer une protestation
+
+Veuillez indiquer le numéro correspondant a l'action que vous souhaitez effectuer"
+
+
