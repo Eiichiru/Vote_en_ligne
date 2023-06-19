26cfc92f18c65846e61791a8ae188aac9afba133
William <william.jorge@etud.u-picardie.fr>
2023-06-19 16:26:35 +0200

    feat/finalisation connexion + fonctions

--- fonction/fonctions.sh
+++ fonction/fonctions.sh
@@ -1,7 +1,8 @@
+source fonction/utils.sh
+
 #openssl genpkey -algorithm RSA -out private.pem
 #openssl rsa -in private.pem -pubout -out public.pem
 #openssl rand -hex 16
-
 chiffrement() {
     if [ $# -ne 2 ]; then
         echo "La fonction attend exactement 2 arguments."
@@ -24,7 +25,6 @@ chiffrement() {
     #Suppression des fichiers inutiles
     rm symKey.txt
 }
-
 #chiffrement message.txt public.pem
 
 dechiffrement() {
@@ -48,5 +48,77 @@ dechiffrement() {
     rm encrypted_key.bin
 
 }
-
 #dechiffrement encrypted_key.bin encrypted_message.txt private.pem
+
+sendPublicKey() {
+    #Creation des clés si elles n'existes pas 
+    if [ ! -f "publicKey/private_Server_key.pem" ]; then
+        openssl genpkey -algorithm RSA -out publicKey/private_Server_key.pem
+    fi 
+    if [ ! -f "public_ssh_key.pub" ]; then
+        openssl rsa -in publicKey/private_Server_key.pem -pubout -out publicKey/public_Server_key.pem
+    fi 
+
+    #envoie de la clé public du server au client 
+    cat publicKey/public_Server_key.pem | send $Client
+    echo "clé envoyé au client"
+
+}
+
+chiffrementMDP() {
+    if [ $# -ne 2 ]; then
+        echo "La fonction attend exactement 2 arguments."
+        return 2
+    fi
+
+    echo $1 > mdp.txt
+
+    #Chiffrement du mdp avec la clé publique du serveur 
+    openssl rsautl -encrypt -pubin -inkey $2 -in mdp.txt -out encrypted_password.bin
+
+    rm mdp.txt
+    cat encrypted_password.bin
+    rm encrypted_password.bin
+}
+#chiffrement $MDP public.pem
+
+dechiffrementMDP() {
+    if [ $# -ne 2 ]; then
+        echo "La fonction attend exactement 2 arguments."
+        return 2
+    fi
+
+    echo $1 > encrypted_password.txt
+
+    #Dechiffrement de la clé symetrique avec la clé privé du serveur 
+    openssl rsautl -decrypt -inkey $2 -in encrypted_password.txt -out decrypted_password.txt
+
+    rm encrypted_password.txt
+    cat decrypted_password.txt
+    rm decrypted_password.txt
+}
+#chiffrement $encrypted_password publicKey/private_Server_key.pem
+
+verifConnexion() {
+    if [ $# -ne 3 ]; then
+        echo "La fonction attend exactement 3 arguments."
+        return 2
+    fi
+    cat database/"$3"_database.txt | while read -r ligne
+    do
+        IDinFile=$(echo "$ligne" | cut -d ':' -f1)
+        MDPinFile=$(echo "$ligne" | cut -d ':' -f9)
+        if [ "$IDinFile" = "$1" ] && [ "$MDPinFile" = "$2" ]; then
+            send $Client <<< "connexion1"
+        else
+            MDPinFile=$(echo "$ligne" | cut -d ':' -f10)
+            if [ "$IDinFile" = "$1" ] && [ "$MDPinFile" = "$2" ]; then
+                send $Client <<< "connexion2"
+            else
+                send $Client <<< "connexion3"
+            fi
+        fi
+    done
+
+}
+#verifConnexion $ID $MDP $ville
