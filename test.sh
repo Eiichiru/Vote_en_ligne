056cfd9f02a68be454571524f5bcbd83836abeac
William <william.jorge@etud.u-picardie.fr>
2023-06-27 23:10:30 +0200

    feat/geturne

--- test.sh
+++ test.sh
@@ -3,4 +3,10 @@
 source fonction/utils4mac.sh
 source fonction/fonctions.sh
 
-verifRightProcu 1234 "amiens" 4321 "26/06/2025"
+getUrne amiens
+
+nbVote1=$(getNbVote amiens)
+echo "nb 1 : "$nbVote1
+
+nbVote2=$(getNbVotebyUrne urne.txt)
+echo "nb 2 : "$nbVote2
